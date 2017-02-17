e0e755d6ec33caeba5564884898b447e9d3244be
Rafael <rclsa@fnal.gov>
2017-02-17 18:16:54 +0100

    correct globaltag

--- Validation/RecoTrack/test/trackingNtupleExample_electronGsfTracks81.py
+++ Validation/RecoTrack/test/trackingNtupleExample_electronGsfTracks81.py
@@ -93,7 +93,7 @@ process.mix.digitizers = cms.PSet()
 for a in process.aliases: delattr(process, a)
 process.RandomNumberGeneratorService.restoreStateLabel=cms.untracked.string("randomEngineStateProducer")
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2017_realistic', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '81X_upgrade2017_realistic_v26', '')
 
 # Path and EndPath definitions
 process.raw2digi_step = cms.Path(process.RawToDigi)
